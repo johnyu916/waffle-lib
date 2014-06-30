3631bb6225d619f656651ec771e4ff28e5e5d850
John Yu <johnyu916@hotmail.com>
2014-06-30 00:22:03 -0400

    json writer plugin

---
+++ python/plugins/jsonsyrup.py
@@ -0,0 +1,7 @@
+import json
+import numpy
+class SyrupEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
